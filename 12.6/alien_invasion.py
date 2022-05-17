ad56f4127f2b2275a1d3d46647a647b130b17901
SamOLee <2252242164@qq.com>
2022-05-17 18:33:40 +0800

    游戏内容(部分)

---
+++ 12.6/alien_invasion.py
@@ -0,0 +1,24 @@
+import sys
+
+import pygame
+
+from settings import Settings
+from ship import Ship
+from bullet import Bullet
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+        pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
+
